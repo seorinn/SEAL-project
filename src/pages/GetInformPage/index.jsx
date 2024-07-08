0c0ee900b733ddb711e2bba1a890ed1e2ae85fc0
seorinn <seorin@ajou.ac.kr>
2024-07-08 13:27:26 +0900

    feat: 전화번호 인증

--- src/pages/GetInformPage/index.jsx
+++ src/pages/GetInformPage/index.jsx
@@ -1,12 +1,37 @@
-import { useNavigate } from "react-router-dom";
-import "./index.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import "./index.css";
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// const getPhoneNumberFromUserInput = (phonenumber) => {
+//   return `+82${phonenumber.slice(1)}`;
+// };
+
+// const auth = getAuth();
 
 function GetInformPage({ userInfo, setUserInfo }) {
   const navigation = useNavigate();
   const [isSended, setIsSended] = useState(false);
   const [submitCode, setSubmitCode] = useState(false);
   const [isCorrectCode, setIsCorrectCode] = useState(false);
+  const [code, setCode] = useState("");
 
   const handleInput = (e) => {
     const { name, value } = e.target;
@@ -16,6 +41,55 @@ function GetInformPage({ userInfo, setUserInfo }) {
     });
   };
 
+  const handleSendCode = () => {
+    // if (!userInfo.phonenumber) return;
+    // setIsSended(true);
+    // window.recaptchaVerifier = new RecaptchaVerifier(auth, "sign-in-button", {
+    //   size: "invisible",
+    //   callback: (response) => {
+    //     //...
+    //   },
+    // });
+    // auth.languageCode = "ko";
+    // const phonenumber = getPhoneNumberFromUserInput(userInfo.phonenumber);
+    // const appVerifier = window.recaptchaVerifier;
+    // signInWithPhoneNumber(auth, phonenumber, appVerifier)
+    //   .then((confirmationResult) => {
+    //     window.confirmationResult = confirmationResult;
+    //   })
+    //   .catch((error) => {
+    // this.firebaseError(error);
+    //     console.log("SMS Failed");
+    //   });
+  };
+
+  const handleCheckCode = () => {
+    // if (!isSended) return;
+    // setSubmitCode(true);
+    // window.confirmationResult
+    //   .confirm(code)
+    //   .then((result) => {
+    //     const user = result.user;
+    //     // ...
+    //     setIsCorrectCode(true);
+    //   })
+    //   .catch((error) => {
+    // this.firebaseError(error);
+    //     console.log(error);
+    //   });
+  };
+
+  const firebaseError = (error) => {
+    if (error.code === "auth/invalid-verification-code") {
+      alert("인증번호가 유효하지 않습니다.");
+    } else if (error.code === "auth/session-expired") {
+      alert("인증번호가 만료되었습니다.");
+    } else if (error.code === "auth/too-many-requests") {
+      alert("잠시 후 다시 시도해 주세요.");
+    } else {
+      alert(error);
+    }
+  };
   const handleSubmit = () => {
     const { name, affiliation, position, phonenumber } = userInfo;
     if (name && affiliation && position && phonenumber && isCorrectCode) {
@@ -58,15 +132,11 @@ function GetInformPage({ userInfo, setUserInfo }) {
           <div className="inputAndButton">
             <input
               name="phonenumber"
-              placeholder="전화번호를 입력해주세요"
+              placeholder="'-' 없이 입력해주세요."
               onChange={handleInput}
             />
-            <button
-              onClick={() => {
-                if (userInfo.phonenumber) setIsSended(true);
-              }}
-            >
-              {isSended ? "인증번호 재요청" : "인증번호 요청"}
+            <button onClick={handleSendCode}>
+              {isSended ? "인증 재요청" : "인증 요청"}
             </button>
           </div>
           {isSended && <span>인증번호가 발송되었습니다.</span>}
@@ -76,20 +146,15 @@ function GetInformPage({ userInfo, setUserInfo }) {
           <div className="inputAndButton">
             <input
               name="code"
-              placeholder="인증번호를 입력해주세요"
+              placeholder="인증번호 6자리를 입력해주세요."
               // onChange={handleInput}
             />
-            <button
-              onClick={() => {
-                if (isSended) setSubmitCode(true);
-              }}
-            >
-              인증번호 확인
-            </button>
+            <button onClick={handleCheckCode}>인증번호 확인</button>
           </div>
           {isSended && submitCode && !isCorrectCode && (
             <span className="wrong-code">인증번호가 일치하지 않습니다.</span>
           )}
+          <div id="recaptcha-container"></div>
         </div>
       </div>
       <button className="btn-submit" onClick={handleSubmit}>
